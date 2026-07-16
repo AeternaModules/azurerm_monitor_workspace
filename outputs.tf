78c81652adb189922ba5bff18b4a5f9ffb7a89b6
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,37 @@
 output "monitor_workspaces_id" {
   description = "Map of id values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.id }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "monitor_workspaces_default_data_collection_endpoint_id" {
   description = "Map of default_data_collection_endpoint_id values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.default_data_collection_endpoint_id }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.default_data_collection_endpoint_id if v.default_data_collection_endpoint_id != null && length(v.default_data_collection_endpoint_id) > 0 }
 }
 output "monitor_workspaces_default_data_collection_rule_id" {
   description = "Map of default_data_collection_rule_id values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.default_data_collection_rule_id }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.default_data_collection_rule_id if v.default_data_collection_rule_id != null && length(v.default_data_collection_rule_id) > 0 }
 }
 output "monitor_workspaces_location" {
   description = "Map of location values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.location }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "monitor_workspaces_name" {
   description = "Map of name values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.name }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "monitor_workspaces_public_network_access_enabled" {
   description = "Map of public_network_access_enabled values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.public_network_access_enabled }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.public_network_access_enabled if v.public_network_access_enabled != null }
 }
 output "monitor_workspaces_query_endpoint" {
   description = "Map of query_endpoint values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.query_endpoint }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.query_endpoint if v.query_endpoint != null && length(v.query_endpoint) > 0 }
 }
 output "monitor_workspaces_resource_group_name" {
   description = "Map of resource_group_name values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.resource_group_name }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "monitor_workspaces_tags" {
   description = "Map of tags values across all monitor_workspaces, keyed the same as var.monitor_workspaces"
-  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.tags }
+  value       = { for k, v in azurerm_monitor_workspace.monitor_workspaces : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
